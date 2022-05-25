732b1b402af211ce8693cc902bd0c39b57b5a110
MarekPrim <59203712+MarekPrim@users.noreply.github.com>
2022-05-25 11:59:01 +0200

    Update StableMariage.java
    factorisation

--- StableMariage.java
+++ StableMariage.java
@@ -9,50 +9,40 @@ public class StableMariage {
 	
 	public static List<Participant> rejetes = new ArrayList<Participant>();
 	
-
-	public static void main(String[] args) {
-		
-		
-		List<Ecole> ecoles = new ArrayList<>();
-		List<Eleve> eleves = new ArrayList<>();
-		
-		ecoles.add(new Ecole("Enseeiht", 3));
-		ecoles.add(new Ecole("Insa", 4));
-		ecoles.add(new Ecole("A7", 3));
-		
-		eleves.add(new Eleve("Kylian"));
-		eleves.add(new Eleve("Keke"));
-		eleves.add(new Eleve("Johan"));
-		eleves.add(new Eleve("Jordan"));
-		eleves.add(new Eleve("Mathias"));
-		eleves.add(new Eleve("Christophe"));
-		eleves.add(new Eleve("Jonathan"));
-		eleves.add(new Eleve("Julien"));
-		eleves.add(new Eleve("Emrick"));
-		eleves.add(new Eleve("Damien"));
-
-		
-		for (Eleve e : eleves) {
-			List<Integer> array = new ArrayList<>();
-		    for (int k = 0; k < ecoles.size(); k++) {
-		        array.add(k);
+	public void creerPreferences(List<? extends Participant> demandeurs, List<? extends Participant> preferences) {
+		for (Participant e : demandeurs) {
+			List<Integer> ordrePreferences = new ArrayList<>();
+		    for (int k = 0; k < preferences.size(); k++) {
+		        ordrePreferences.add(k);
 		    }
-		    Collections.shuffle(array);
-		    for (Integer preference : array) {
-		    	e.ajouterPreference(ecoles.get(preference));
+		    Collections.shuffle(ordrePreferences);
+		    for (Integer preference : ordrePreferences) {
+		    	e.ajouterPreference(preferences.get(preference));
 		    }
 		}
-		
-		for (Ecole ecole : ecoles) {
-			List<Integer> array = new ArrayList<>();
-		    for (int k = 0; k < eleves.size(); k++) {
-		        array.add(k);
-		    }
-		    Collections.shuffle(array);
-		    for (Integer preference : array) {
-		    	ecole.ajouterPreference(eleves.get(preference));
-		    }
+	}
+	
+	public void associerDemandeurDemande(List<? extends Participant> demandeurs, List<? extends Participant> demandes, int nbTours) {
+		for (Participant demande : demandes) {
+			for (Participant demandeur : demandeurs) {
+				if (demandeur.getPreferences().get(0) == demande) {
+					demande.ajouterDemandeur(demandeur);
+					System.out.println(demandeur.getNom() + " a choisi " + demande.getNom() + " au bout de " + nbTours + " tours");
+				}
+			}
 		}
+	}
+	
+	public void trierDemandeurs(List<? extends Participant> demandes) {
+		for (Participant demande : demandes) {
+			demande.trierDemandeurs();
+		}
+	}
+	
+	public void resoudre(List<? extends Participant> demandeurs, List<? extends Participant> demandes) {
+		
+		this.creerPreferences(demandeurs, demandes);
+		this.creerPreferences(demandes, demandeurs);
 		
 		int nbTours = 0;
 		
@@ -69,19 +59,10 @@ public class StableMariage {
 //				System.out.println(ecole);
 //			}
 			
-			for (Ecole ecole : ecoles) {
-				for (Eleve eleve : eleves) {
-					if (eleve.getPreferences().get(0) == ecole) {
-						ecole.ajouterDemandeur(eleve);
-						System.out.println(eleve.getNom() + " a choisi " + ecole.getNom() + " au bout de " + nbTours + " tours");
-					}
-				}
-			}
+			this.associerDemandeurDemande(demandeurs, demandes, nbTours);
+			this.trierDemandeurs(demandes);
 			
 			
-			for (Ecole ecole : ecoles) {
-				ecole.trierDemandeurs();
-			}
 //			
 //			for (Ecole ecole : ecoles) {
 //				ecole.afficherDemandeurs();
@@ -99,46 +80,44 @@ public class StableMariage {
 			nbTours++;
 			
 		} while(!rejetes.isEmpty());
+	}
+
+	public static void main(String[] args) {
 		
-//		Preference[][] preferences = new Preference[eleves.size()][ecoles.size()]; 
-//		
-//		
-//		for (int i = 0; i < eleves.size(); i++) {
-//			List<Integer> array = new ArrayList<>();
-//		    for (int k = 0; k < ecoles.size(); k++) {
-//		        array.add(k);
-//		    }
-//		    Collections.shuffle(array);
-//			 for (int j = 0; j < ecoles.size(); j++) {
-//				 preferences[i][j] = new Preference(array.get(0), 0);
-//				 array.remove(0);
-//			 }
-//		 }
-//		
-//		 for (int i = 0; i < ecoles.size(); i++) {
-//			 List<Integer> array = new ArrayList<>();
-//			    for (int k = 0; k < eleves.size(); k++) {
-//			        array.add(k);
-//			    }
-//			 Collections.shuffle(array);
-//			 for (int j = 0; j < eleves.size(); j++) {
-//			    preferences[j][i].setPreferenceEcole(array.get(0));
-//		    	array.remove(0);
-//			 }
-//		 }
-//		 
-//		 for (int i = 0; i < eleves.size(); i++) {
-//			 for (int j = 0; j < ecoles.size(); j++) {
-//				 System.out.print(preferences[i][j]);
-//				 if (j < ecoles.size()-1) {
-//					 System.out.print(" - ");
-//				 }
-//			 }
-//			 System.out.println();
-//		 }
-		 
-		 
-		 
+		StableMariage s = new StableMariage();
+		
+		
+		List<Ecole> ecoles = new ArrayList<>();
+		List<Eleve> eleves = new ArrayList<>();
+		
+		ecoles.add(new Ecole("Enseeiht", 6));
+		ecoles.add(new Ecole("Insa", 8));
+		ecoles.add(new Ecole("A7", 6));
+		
+		eleves.add(new Eleve("Kylian"));
+		eleves.add(new Eleve("Keke"));
+		eleves.add(new Eleve("Johan"));
+		eleves.add(new Eleve("Jordan"));
+		eleves.add(new Eleve("Mathias"));
+		eleves.add(new Eleve("Christophe"));
+		eleves.add(new Eleve("Jonathan"));
+		eleves.add(new Eleve("Julien"));
+		eleves.add(new Eleve("Emrick"));
+		eleves.add(new Eleve("Damien"));
+		
+		eleves.add(new Eleve("A"));
+		eleves.add(new Eleve("B"));
+		eleves.add(new Eleve("C"));
+		eleves.add(new Eleve("D"));
+		eleves.add(new Eleve("E"));
+		eleves.add(new Eleve("F"));
+		eleves.add(new Eleve("G"));
+		eleves.add(new Eleve("H"));
+		eleves.add(new Eleve("I"));
+		eleves.add(new Eleve("J"));
+
+		s.resoudre(ecoles, eleves);
+		 	 
 	}
 
 }
